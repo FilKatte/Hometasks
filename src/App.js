c2adc51d4a4c478b2cf60f7019d9029b6a2a21b9
Филина Екатерина <filinaekaterina@Macmini-Barinov.local>
2019-07-29 16:24:47 +0300

    add test case

--- src/App.js
+++ src/App.js
@@ -2,51 +2,8 @@ import React from "react";
 import MessageItem from "./MessageItem";
 import "./App.css";
 
-class Message extends React.Component {
-  state = {
-    value: "",
-    message: []
-  };
-
-  handleChange = event => {
-    this.setState({ value: event.target.value });
-  };
-
-  handleSubmit = event => {
-    const { value, message } = this.state;
-    event.preventDefault();
-    value &&
-      this.setState({
-        message: [...message, { text: value }],
-        value: ""
-      });
-  };
-
-  render() {
-    const { message } = this.state;
-
-    return (
-      <div>
-        <ul>
-          {message.map((mes, i) => (
-            <MessageItem key={i} text={mes.text} />
-          ))}
-        </ul>
-
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Сообщение
-            <input
-              type="text"
-              value={this.state.value}
-              onChange={this.handleChange}
-            />
-          </label>
-          <input type="submit" value="Отправить" />
-        </form>
-      </div>
-    );
-  }
-}
+const Message = () => {
+  return <div>hello</div>;
+};
 
 export default Message;
