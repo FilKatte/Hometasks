d76b13412694e8477de7f8dec0b0ed7ebc711a4b
Филина Екатерина <filinaekaterina@Macmini-Barinov.local>
2019-07-30 13:11:01 +0300

    Show data, show css

--- src/components/Show/Show.js
+++ src/components/Show/Show.js
@@ -5,16 +5,38 @@ import { getShowInfo } from "./../../api";
 
 class Show extends React.Component {
   state = { data: null };
+
   componentDidUpdate(prevProps, prevState) {
     const { showId } = this.props;
-    console.log("PREV ", prevProps.showId, "CURENT ", this.props.showId);
+
     if (prevProps.showId !== this.props.showId) {
       getShowInfo(showId).then(data => this.setState({ data }));
     }
   }
   render() {
-    const { showId } = this.props;
-    return <div>{showId}</div>;
+    const { data } = this.state;
+
+    return (
+      <div>
+        {data && (
+          <div className="serial">
+            <div className="icon">
+              <img src={data.image.medium} alt="Cover" />
+            </div>
+            <div className="info">
+              <div className="name">{data.name}</div>
+              <div>
+                <span className="genres">Жанр:</span> {data.genres.join(", ")}
+              </div>
+              <div
+                className="summary"
+                dangerouslySetInnerHTML={{ __html: data.summary }}
+              />
+            </div>
+          </div>
+        )}
+      </div>
+    );
   }
 }
 
