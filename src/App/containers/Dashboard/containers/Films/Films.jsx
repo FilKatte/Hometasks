3646666f9cd26fa1fab132c6e633e91174559995
FillKatte <filina-kate@mail.ru>
2019-10-18 16:27:19 +0300

    films style + animation

--- src/App/containers/Dashboard/containers/Films/Films.jsx
+++ src/App/containers/Dashboard/containers/Films/Films.jsx
@@ -4,8 +4,8 @@ import { filmsListSelector, loaderFilmsListSelector } from "./store/selectors";
 import { getFilmList, sortAsFilmList, sortDesFilmList } from "./store/duck";
 import styles from "./Films.module.css";
 import SearchSelect from "../../components/SearchSelect";
-import { CSSTransition, TransitionGroup } from "react-transition-group";
-import slide from "./slide.module.css";
+import { CSSTransition } from "react-transition-group";
+import "./style.css";
 
 const mapStateToProps = state => {
   return {
@@ -44,55 +44,53 @@ class Films extends React.Component {
     return (
       <div className={styles.films}>
         <div className={styles.title}>Films</div>
-        <div className={styles.sort}>
-          <p className={styles.sort_text}>Sort</p>
-          <div className={styles.sort_buttons}>
-            <button
-              onClick={this.handleClickSortAs}
-              className={styles.sort_button}
-            />
-            <button
-              onClick={this.handleClickSortDes}
-              className={styles.sort_button}
-            />
+        <div className={styles.options}>
+          <div className={styles.films_select}>
+            <SearchSelect filmsList={filmsList} />
+          </div>
+          <div className={styles.sort}>
+            <div className={styles.sort_buttons}>
+              <button
+                onClick={this.handleClickSortAs}
+                className={styles.sort_button}
+              />
+              <button
+                onClick={this.handleClickSortDes}
+                className={styles.sort_button}
+              />
+            </div>
           </div>
         </div>
 
-        <div className={styles.films_select}>
-          <SearchSelect filmsList={filmsList} />
-        </div>
-        {loading ? (
+        {loading && (
           <div className={styles.loading}>
             <div className={styles.loader} />
             <p className={styles.text}>Loading</p>
           </div>
-        ) : (
-          filmsList && (
+        )}
+        {filmsList && (
+          <CSSTransition
+            in={filmsList.length > 0}
+            timeout={1000}
+            classNames="my-node"
+          >
             <ul className={styles.films_list}>
-              <TransitionGroup>
-                {filmsList.map(obj => (
-                  <CSSTransition
-                    key={obj.episode_id}
-                    timeout={2000}
-                    classNames={slide}
-                  >
-                    <li key={obj.episode_id} className={styles.films_item}>
-                      <input
-                        type="checkbox"
-                        id={obj.episode_id}
-                        className={styles.hide}
-                      />
-                      <label htmlFor={obj.episode_id}>
-                        {obj.title}, episode {obj.episode_id},{" "}
-                        {obj.release_date.slice(0, 4)}
-                      </label>
-                      <p className={styles.films_text}>{obj.opening_crawl}</p>
-                    </li>
-                  </CSSTransition>
-                ))}
-              </TransitionGroup>
+              {filmsList.map(obj => (
+                <li key={obj.episode_id} className={styles.films_item}>
+                  <input
+                    type="checkbox"
+                    id={obj.episode_id}
+                    className={styles.hide}
+                  />
+                  <label htmlFor={obj.episode_id}>
+                    {obj.title}, episode {obj.episode_id},{" "}
+                    {obj.release_date.slice(0, 4)}
+                  </label>
+                  <p className={styles.films_text}>{obj.opening_crawl}</p>
+                </li>
+              ))}
             </ul>
-          )
+          </CSSTransition>
         )}
       </div>
     );
