caaf7c43bc144736cfb393322143ca1fc7f634a5
FillKatte <filina-kate@mail.ru>
2019-10-18 12:27:35 +0300

    test transition

--- src/App/containers/Dashboard/containers/Films/Films.jsx
+++ src/App/containers/Dashboard/containers/Films/Films.jsx
@@ -4,6 +4,8 @@ import { filmsListSelector, loaderFilmsListSelector } from "./store/selectors";
 import { getFilmList, sortAsFilmList, sortDesFilmList } from "./store/duck";
 import styles from "./Films.module.css";
 import SearchSelect from "../../components/SearchSelect";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import slide from "./slide.module.css";
 
 const mapStateToProps = state => {
   return {
@@ -38,13 +40,8 @@ class Films extends React.Component {
 
   render() {
     const { filmsList, loading } = this.props;
-
-    return loading ? (
-      <div className={styles.loading}>
-        <div className={styles.loader} />
-        <p className={styles.text}>Loading</p>
-      </div>
-    ) : (
+    console.log("render");
+    return (
       <div className={styles.films}>
         <div className={styles.title}>Films</div>
         <div className={styles.sort}>
@@ -64,23 +61,38 @@ class Films extends React.Component {
         <div className={styles.films_select}>
           <SearchSelect filmsList={filmsList} />
         </div>
-        {filmsList && (
-          <ul className={styles.films_list}>
-            {filmsList.map(obj => (
-              <li key={obj.episode_id} className={styles.films_item}>
-                <input
-                  type="checkbox"
-                  id={obj.episode_id}
-                  className={styles.hide}
-                />
-                <label htmlFor={obj.episode_id}>
-                  {obj.title}, episode {obj.episode_id},{" "}
-                  {obj.release_date.slice(0, 4)}
-                </label>
-                <p className={styles.films_text}>{obj.opening_crawl}</p>
-              </li>
-            ))}
-          </ul>
+        {loading ? (
+          <div className={styles.loading}>
+            <div className={styles.loader} />
+            <p className={styles.text}>Loading</p>
+          </div>
+        ) : (
+          filmsList && (
+            <ul className={styles.films_list}>
+              <TransitionGroup>
+                {filmsList.map(obj => (
+                  <CSSTransition
+                    key={obj.episode_id}
+                    timeout={2000}
+                    classNames={slide}
+                  >
+                    <li key={obj.episode_id} className={styles.films_item}>
+                      <input
+                        type="checkbox"
+                        id={obj.episode_id}
+                        className={styles.hide}
+                      />
+                      <label htmlFor={obj.episode_id}>
+                        {obj.title}, episode {obj.episode_id},{" "}
+                        {obj.release_date.slice(0, 4)}
+                      </label>
+                      <p className={styles.films_text}>{obj.opening_crawl}</p>
+                    </li>
+                  </CSSTransition>
+                ))}
+              </TransitionGroup>
+            </ul>
+          )
         )}
       </div>
     );
