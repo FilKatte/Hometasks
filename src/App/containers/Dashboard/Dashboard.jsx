bdc2e0955568c5701f55883a7002e0e56b5f84f3
FillKatte <filina-kate@mail.ru>
2019-11-06 11:26:16 +0300

    add cursor follower

--- src/App/containers/Dashboard/Dashboard.jsx
+++ src/App/containers/Dashboard/Dashboard.jsx
@@ -2,17 +2,21 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Films from "./containers/Films";
 import People from "./containers/People";
+import CursorFollower from "../../../shared/CursorFollower";
 import styles from "./Dashboard.module.css";
 
 class Dashboard extends React.Component {
   render() {
     return (
       <div className={styles.dashboard}>
-        <Switch>
-          {/* <Route path="/dashboard/films" component={Films} /> */}
-          <Route path="/dashboard/people" component={People} />
-          <Route path="/dashboard" component={Films} />
-        </Switch>
+        <div className={styles.content}>
+          <Switch>
+            <Route path="/dashboard/films" component={Films} />
+            <Route path="/dashboard/people" component={People} />
+            <Route path="/dashboard" component={Films} />
+          </Switch>
+        </div>
+        <CursorFollower />
       </div>
     );
   }
---
+++ src/shared/CursorFollower/CursorFollower.js
@@ -0,0 +1,31 @@
+import React from "react";
+import styles from "./CursorFollower.module.css";
+
+class CursorFollower extends React.Component {
+  state = {
+    x: 0,
+    y: 0
+  };
+
+  componentDidMount() {
+    window.addEventListener("mousemove", this.handleMouseMove);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("mousemove", this.handleMouseMove);
+  }
+
+  handleMouseMove = event => {
+    this.setState({
+      x: event.clientX - 10,
+      y: event.clientY - 100
+    });
+  };
+
+  render() {
+    const { x, y } = this.state;
+    return <div className={styles.star} style={{ top: y, left: x }}></div>;
+  }
+}
+
+export default CursorFollower;
